aa63fdd682a574a98209f4d0e6938e8b99e413c6
dkwazhere <dhealth77@gmail.com>
2018-02-02 02:53:49 -0800

    cleaned up the code, added functions if users do not type anything after command inputs

--- liri.js
+++ liri.js
@@ -6,23 +6,15 @@ var Twitter = require('twitter');
 var Spotify = require('node-spotify-api');
 var input = process.argv[2];
 
-// Twitter access.
-var client = new Twitter({
-  consumer_key: process.env.TWITTER_CONSUMER_KEY,
-  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
-});
-// Spotify access.
-
-var spotify = new Spotify({
-  id: process.env.SPOTIFY_ID,
-  secret: process.env.SPOTIFY_SECRET
-});
-
 // Functions
 function tweets() {
     var params = {screen_name: 'TheNotoriousMMA'};
+    var client = new Twitter({
+      consumer_key: process.env.TWITTER_CONSUMER_KEY,
+      consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+      access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+      access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+    });
     client.get('statuses/user_timeline', params, function(error, tweets, response) {
       if (!error) {
         console.log(tweets);
@@ -32,15 +24,37 @@ function tweets() {
       }
     });
 }
+
 function spotify() {
+    var spotify = new Spotify({
+      id: process.env.SPOTIFY_ID,
+      secret: process.env.SPOTIFY_SECRET
+      });
     spotify.search({ type: 'track', query: process.argv[3] }, function(err, data) {
         if (err) {
             return console.log('Error occurred: ' + err);
+        } else {
+            console.log("Artist Name: "+ data.tracks.items[0].artists[0].name);
+            console.log("Song Name: " + data.tracks.items[0].name);
+            console.log("Album name: " + data.tracks.items[0].album.name);
+            console.log("URL for song: " + data.tracks.items[0].external_urls.spotify);
         }
-        console.log("Artist Name: "+ data.tracks.items[0].artists[0].name);
-        console.log("Song Name: " + data.tracks.items[0].name);
-        console.log("Album name: " + data.tracks.items[0].album.name);
-        console.log("URL for song: " + data.tracks.items[0].external_urls.spotify);
+    });
+}
+function spotifyNull() {
+    var spotify = new Spotify({
+      id: process.env.SPOTIFY_ID,
+      secret: process.env.SPOTIFY_SECRET
+      });
+    spotify.search({ type: 'track', query: 'The Sign' }, function(err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        } else {
+            console.log("Artist Name: "+ data.tracks.items[5].artists[0].name);
+            console.log("Song Name: " + data.tracks.items[5].name);
+            console.log("Album name: " + data.tracks.items[5].album.name);
+            console.log("URL for song: " + data.tracks.items[5].external_urls.spotify);
+        }
     });
 }
 
@@ -61,13 +75,38 @@ request("http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy", 
 });
 }
 
+function omdbNull() {
+var request = require("request");
+var movie = process.argv[3];
+request("http://www.omdbapi.com/?t=Mr.Nobody&y=&plot=short&apikey=trilogy", function(error, response, body) {
+    if (!error && response.statusCode === 200) {
+        console.log("Title: " + JSON.parse(body).Title);
+        console.log("Release date: " + JSON.parse(body).Released);
+        console.log("imdb Rating: " + JSON.parse(body).imdbRating);
+        console.log(JSON.parse(body).Ratings[1].Source + " Rating: " + JSON.parse(body).Ratings[1].Value);
+        console.log("Country of production is: " + JSON.parse(body).Country);
+        console.log("Languages: " + JSON.parse(body).Language);
+        console.log("Plot: " + JSON.parse(body).Plot);
+        console.log("Actors: " + JSON.parse(body).Actors);
+    }
+});
+}
+
 // User commands conditionals.
 if (input === "my-tweets") {
     tweets();
 } else if (input === "spotify-this-song") {
-    spotify();
+    if (process.argv[3] === undefined) {
+        spotifyNull();
+    } else {
+        spotify();
+    }
 } else if (input === "movie-this") {
-    omdb();
+    if (process.argv[3] === undefined) {
+        omdbNull();
+    } else {
+        omdb();
+    }
 } else if (input === "do-what-it-says") {
     console.log("I-want-it-that-way");
 } else {
